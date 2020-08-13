7cfac53e1dac5283af4fb55e6c589d3dcd1df728
Colton Conroy <coltonjconroy@gmail.com>
2020-08-13 15:55:46 -0400

    Update write_input_parameter.m

--- write_input_parameter.m
+++ write_input_parameter.m
@@ -14,11 +14,9 @@ wall_bc_type = 1;        % 0 = no slip, 1 = no normal flow
 rho  = 1350;        % density
 hint = 10.0;        % steady thickness h 
 n    = 1.750;       % power law exponent in nonlinear viscosity relation
-A    = -4.55d0;     % for n = 1
-B    = 5574.2;      % for n = 1
-C    = 609.4;       % for n = 1
-b0   = 2.73e4;      % experimental reference temperature (for n neq 1)
-mu0  = 6.025e-7;    % viscosity @ infinite temperature   (for n neq 1)
+A    = -4.55d0;     % coefficient for viscosity model
+B    = 5574.2;      % " "
+C    = 609.4;       % " "
 tau_yield = 0;      % yield strength 
 
 % Thermal parameters
